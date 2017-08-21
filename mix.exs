95c161afc95da68a38b9555f6ee7d34e5a9646ac
Lei Ting <leiting@firevale.com>
2017-08-21 21:22:50 +0800

    hex publish

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule RedixCluster.Mixfile do
      build_embedded: Mix.env in [:prod],
      start_permanent: Mix.env == :prod,
      preferred_cli_env: [espec: :test],
-     deps: deps()]
+     deps: deps,
+     description: description, 
+     package: package]
   end
 
   # Configuration for the OTP application
@@ -31,13 +33,27 @@ defmodule RedixCluster.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [ {:redix, "~> 0.6.0"},
-      {:poolboy, "~> 1.5", override: true},
+      {:poolboy, "~> 1.5"},
       {:dialyze, "~> 0.2", only: :dev},
       {:dogma, "~> 0.0", only: :dev},
       {:crc, "~> 0.5"},
       {:benchfella, github: "alco/benchfella", only: :bench},
       {:eredis_cluster, github: "adrienmo/eredis_cluster", only: :bench},
       {:espec, github: "antonmi/espec", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev}, 
     ]
   end
+
+  defp description do 
+    "A wrapper for redix to support cluster mode of redis"
+  end
+
+  defp package do
+     [
+       maintainers: ["zhongwencool", "masahiro tokioka", "tinglei8"],
+       licenses: ["MIT"],
+       links: %{"GitHub" => "https://github.com/tinglei8/redix-cluster.git"}
+     ]
+  end
+
 end
